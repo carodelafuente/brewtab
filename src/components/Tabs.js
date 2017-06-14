a8a10a9a760343a74297fe0f61f6488239a61d48
Carolina de la Fuente <mdela160@fiu.edu>
2017-06-13 21:26:24 -0400

    fixed tabs component

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -32,18 +32,32 @@ class Tabs extends Component {
     }
   }
 
+  // render () {
+  //   return <div className='Tabs BeerList'>
+  //     <span> Your Tabs </span>
+  //     <Carousel dragging className='carousel'>
+  //       {this.state.tabs.map((tab) => {
+  //         return <div key={tab.id} className='beerSlide'>
+  //           <h4>{tab.beers[0].name}</h4>
+  //           <img className='logo' src={tab.beers[0].logo} />
+  //           <h4> ABV: {tab.beers[0].abv} </h4>
+  //         </div>
+  //       })}
+  //     </Carousel>
+  //   </div>
+  // }
+
   render () {
     return <div className='Tabs BeerList'>
       <span> Your Tabs </span>
-      <Carousel dragging className='carousel'>
-        {this.state.tabs.map((tab) => {
-          return <div key={tab.id} className='beerSlide'>
-            <h4>{tab.beers[0].name}</h4>
-            <img className='logo' src={tab.beers[0].logo} />
-            <h4> ABV: {tab.beers[0].abv} </h4>
-          </div>
-        })}
-      </Carousel>
+      {this.state.tabs.map((tab) => {
+        return <div key={tab.id} className='beerBox'>
+          <img className='logoBox' src={tab.beers[0].logo} />
+          <h4 className='name'> {tab.beers[0].name} </h4>
+          <h4 className='abv'> <div className='abv2'> ABV: </div> {tab.beers[0].abv} </h4>
+          {/* <a href='#'> Read More </a> */}
+        </div>
+      })}
     </div>
   }
 }
